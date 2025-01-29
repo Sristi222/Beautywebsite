1bb5138a31b74ef93f0993f73c6294cee99efa8a
Sristi Shrestha <sristishrestha80@gmail.com>
2025-01-29 15:30:44 +0545

    First commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/Sristi222/Beautywebsite.git',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
+
